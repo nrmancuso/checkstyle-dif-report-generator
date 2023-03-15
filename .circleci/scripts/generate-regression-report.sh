51b7be0ccb6dc45852c22a5d39f37bd2756403b8
Nick Mancuso <nicholas.mancuso13@gmail.com>
2023-03-15 00:48:56 -0400

    infra: begin clone of repos

--- .circleci/scripts/generate-regression-report.sh
+++ .circleci/scripts/generate-regression-report.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
+set -e
 # Usage: Automate check behavior regression report
 
 cd contribution/checkstyle-tester || exit 1
 
+CS_REPO_PATH="../../${CHECKSTYLE_DIRECTORY}"
+CONFIG_PATH="../../configs/${CONFIG_FILE}"
+PROJECTS_PATH="../../projects/projects-to-test-on.properties"
+
+if [ ! -d "$CS_REPO_PATH" ]; then
+  echo "$CS_REPO_PATH does not exist."
+fi
+
 groovy diff.groovy \
-  -r "../../${CHECKSTYLE_DIRECTORY}" \
-  -b master -p "$PATCH_BRANCH" \
-  -c "../../configs/$CONFIG_FILE" \
-  -l ../../projects/projects-to-test-on.properties
+  -r "$CS_REPO_PATH" \
+  -b master \
+  -p "$PATCH_BRANCH" \
+  -c "$CONFIG_PATH" \
+  -l "$PROJECTS_PATH" \
+  -xm "-Dcheckstyle.failsOnError=false" \
+  --allowExcludes
